3a2b3fde35cd9ddc1c1d8ba4de41681ed4a9e1f0
Acácio S. Costa <acaciosantossi@gmail.com>
2019-10-07 01:26:04 -0300

    alter route /active/:id to /active/:hash

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,6 @@ const UserController = require('./controllers/UserController') // Importa arquiv
 
 routes.post('/auth', UserController.auth) // Rota post para autenticação - executa função auth de UserController
 routes.post('/register', UserController.register) // Rota post para cadastro - executa função register de UserController
-routes.get('/active/:id', UserController.active) // Rota get para ativar conta do usuário - executa função active de UserController
+routes.get('/active/:hash', UserController.active) // Rota get para ativar conta do usuário - executa função active de UserController
 
 module.exports = routes
